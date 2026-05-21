656e951aad59b2afcefb29e2dcee783b20435177
JeanHokayem-STUDENT <JeanHokayem-STUDENT@S-7M1H504-SL04>
2026-05-21 13:13:12 +0100

    classes creation questions c d

---
+++ OODExam_2026_S00295656/Member.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OODExam_2026_S00295656
+{
+    internal class Member
+    {
+        public int MemberId { get; set; }
+        public string FirstName { get; set; }
+        public string Surname { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string ContactNumber { get; set; }
+        public string MembershipType { get; set; }
+    }
+}
---
+++ OODExam_2026_S00295656/TrainingSession.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OODExam_2026_S00295656
+{
+    internal class TrainingSession
+    {
+        public int SessionId { get; set; }
+        public DateTime Sessiondate { get; set; }
+        public string SessionType { get; set; }
+        public int DurationMinutes { get; set; }
+        public string CoachNotes { get; set; }
+    }
+}
