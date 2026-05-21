7619690a0bc7224f6e60f6e25688efb5f3ee7810
JeanHokayem-STUDENT <JeanHokayem-STUDENT@S-7M1H504-SL04>
2026-05-21 13:06:36 +0100

    Add project files.

---
+++ OODExam_2026_S00295656/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace OODExam_2026_S00295656
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
